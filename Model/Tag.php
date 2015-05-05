1cbe9c1a2bf4ff78b1a6a2aa703ffd19425b2790
RyujiAMANO <ryuji@ryus.co.jp>
2015-05-05 11:01:14 +0900

    ブログプラグイン中のタグ用コードをTagsプラグインへ

--- Model/Tag.php
+++ Model/Tag.php
@@ -96,4 +96,95 @@ class Tag extends TagsAppModel {
 			'order' => ''
 		)
 	);
+
+	// OK
+	public function getTagsByEntryId($entryId) {
+		$conditions = array(
+			'BlogEntryTagLink.blog_entry_id' => $entryId,
+		);
+		$options = array(
+			'conditions' => $conditions,
+		);
+		$options['joins'] = array(
+			array('table' => 'blog_entry_tag_links',
+				'alias' => 'BlogEntryTagLink',
+				'type' => 'LEFT',
+				'conditions' => array(
+					'BlogTag.id = BlogEntryTagLink.blog_tag_id',
+				)
+			)
+		);
+		$tags = $this->find('all', $options);
+		return $tags;
+	}
+
+	// OK
+	public function getTagsListByEntryId($entryId) {
+		$tags = $this->getTagsByEntryId($entryId);
+		$list = array();
+		foreach ($tags as $tag) {
+			$list[] = $tag['BlogTag'];
+		}
+		return $list;
+
+	}
+
+	// OK
+	public function saveTags($blockId, $entryId, $tags) {
+		if (!is_array($tags)) {
+			$tags = array();
+		}
+		// 存在しないタグを保存
+		// タグへのリンクを保存
+		$tagNameList = array();
+		foreach ($tags as $tag) {
+			if (isset($tag['name'])) {
+				$tagNameList[] = $tag['name'];
+			}
+		}
+		foreach ($tagNameList as $tagName) {
+			//
+			$savedTag = $this->findByBlockIdAndName($blockId, $tagName);
+			if (!$savedTag) {
+				// $tagがないなら保存
+				$data = $this->create();
+
+				$data['BlogTag']['name'] = $tagName;
+				$data['BlogTag']['block_id'] = $blockId;
+				if ($this->save($data)) {
+					$savedTag = $this->findById($this->id);
+				} else {
+					return false;
+				}
+			}
+			// save link
+			$link = $this->BlogEntryTagLink->create();
+			$link['BlogEntryTagLink']['blog_entry_id'] = $entryId;
+			$link['BlogEntryTagLink']['blog_tag_id'] = $savedTag['BlogTag']['id'];
+
+			if (!$this->BlogEntryTagLink->save($link)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * origin_idがセットされてなかったらorigin_id=idとしてアップデート
+	 *
+	 * @param bool $created created
+	 * @param array $options options
+	 * @return void
+	 */
+	public function afterSave($created, $options = array()) {
+		if ($created) {
+			if (empty($this->data[$this->name]['origin_id'])) {
+				// origin_id がセットされてなかったらkey=idでupdate
+				$this->originId = $this->data[$this->name]['id'];
+				$this->saveField('origin_id', $this->data[$this->name]['id'], array('callbacks' => false)); // ここで$this->dataがリセットされる
+			}
+		}
+	}
+
+
 }
