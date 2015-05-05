8326ec836a64aaaa9d1a56e42f0b13193046b485
RyujiAMANO <ryuji@ryus.co.jp>
2015-05-05 10:53:04 +0900

    bakeだけ

---
+++ Model/Tag.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Tag Model
+ *
+ * @property Block $Block
+ * @property Origin $Origin
+ * @property Language $Language
+ *
+* @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+* @link     http://www.netcommons.org NetCommons Project
+* @license  http://www.netcommons.org/license.txt NetCommons License
+ */
+
+App::uses('TagsAppModel', 'Tags.Model');
+
+/**
+ * Summary for Tag Model
+ */
+class Tag extends TagsAppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'block_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'model' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'origin_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'name' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Block' => array(
+			'className' => 'Block',
+			'foreignKey' => 'block_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Origin' => array(
+			'className' => 'Origin',
+			'foreignKey' => 'origin_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Language' => array(
+			'className' => 'Language',
+			'foreignKey' => 'language_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
---
+++ Test/Fixture/TagFixture.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * TagFixture
+ *
+* @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+* @link     http://www.netcommons.org NetCommons Project
+* @license  http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Summary for TagFixture
+ */
+class TagFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary', 'comment' => 'ID |  |  | '),
+		'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'comment' => 'block id |  ブロックID | blocks.id | '),
+		'model' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'origin_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'comment' => 'tag key | タグKey |  | '),
+		'language_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'comment' => 'tag name | タグ名 |  | ', 'charset' => 'utf8'),
+		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'created user | 作成者 | users.id | '),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'created datetime | 作成日時 |  | '),
+		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'modified user | 更新者 | users.id | '),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'modified datetime | 更新日時 |  | '),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'block_id' => 1,
+			'model' => 'Lorem ipsum dolor sit amet',
+			'origin_id' => 1,
+			'language_id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created_user' => 1,
+			'created' => '2015-05-05 01:32:05',
+			'modified_user' => 1,
+			'modified' => '2015-05-05 01:32:05'
+		),
+	);
+
+}
