9ce1e8beb4dd81593c88c183759b53a9df2a03a5
RyujiAMANO <ryuji@ryus.co.jp>
2015-05-07 19:19:23 +0900

    add AllTagsTest.php

---
+++ Test/Case/AllTagsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tags All Test Suite
+ *
+ * @author Ryuji AMANO <ryuji@ryus.co.jp>
+ * @package NetCommons\Tags\Test\Case
+ * @codeCoverageIgnore
+ */
+class AllTagsTest extends CakeTestSuite {
+
+/**
+ * All test suite
+ *
+ * @return CakeTestSuite
+ */
+	public static function suite() {
+		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
+		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
+		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
+		return $suite;
+	}
+}
