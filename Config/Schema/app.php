8516ed4d3dad759cc351f6c036f0f5a030cc21f5
RyujiAMANO <ryuji@ryus.co.jp>
2015-05-14 19:56:43 +0900

    add migrations

--- Config/Schema/app.php
+++ Config/Schema/app.php
@@ -1,35 +1,13 @@
-<?php
-/**
- * schema
- */
-
-/**
- * Class AppSchema
- */
+<?php 
 class AppSchema extends CakeSchema {
 
-/**
- * before
- *
- * @param array $event event
- * @return bool
- */
 	public function before($event = array()) {
 		return true;
 	}
 
-/**
- * after
- *
- * @param array $event event
- * @return void
- */
 	public function after($event = array()) {
 	}
 
-/**
- * @var array tags
- */
 	public $tags = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary', 'comment' => 'ID |  |  | '),
 		'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'comment' => 'block id |  ブロックID | blocks.id | '),
@@ -37,6 +15,21 @@ class AppSchema extends CakeSchema {
 		'origin_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'comment' => 'tag key | タグKey |  | '),
 		'language_id' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'comment' => 'tag name | タグ名 |  | ', 'charset' => 'utf8'),
+		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'created user | 作成者 | users.id | '),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'created datetime | 作成日時 |  | '),
+		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'modified user | 更新者 | users.id | '),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'modified datetime | 更新日時 |  | '),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $tags_contents = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'model' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'content_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 45),
+		'tag_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 45),
 		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'created user | 作成者 | users.id | '),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'created datetime | 作成日時 |  | '),
 		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'modified user | 更新者 | users.id | '),
